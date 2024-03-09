52b57b2797b258dc77d0b479bdecf6d6dba3d586
huly <qq6308qq@gmail.com>
2024-03-09 21:20:55 +0800

    chore: create Navbar prototype

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Navbar from "@/components/shared/navbar/Navbar";
 import { useTheme } from "@/context/ThemeProvider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -7,7 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <main className="background-light850_dark100 relative">
-      Navbar
+      <Navbar />
       <div className="flex">
         LeftSidebar
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
