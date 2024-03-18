d0d0f52702818abb5a8011d4cc2fdf74c000a2e9
huly <qq6308qq@gmail.com>
2024-03-19 00:37:04 +0800

    chore: Questions section && Tags section

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,21 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const hotQuestions = [
+  { _id: 1, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 2, title: "Cascading Deletes in SQLAlchemy?" },
+  { _id: 3, title: "How to Perfectly Center a Div with Tailwind CSS?" },
+  {
+    _id: 4,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  },
+  { _id: 5, title: "Redux Toolkit Not Updating State as Expected" },
+];
+
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 5 },
+  { _id: 3, name: "next", totalQuestions: 5 },
+  { _id: 4, name: "vue", totalQuestions: 2 },
+  { _id: 5, name: "redux", totalQuestions: 10 },
+];
+
 const RightSidebar = () => {
   return (
-    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-[100svh] flex-col justify-between border-l px-6 pt-36 max-lg:hidden lg:w-[330px]">
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-[100svh] w-[350px] flex-col justify-between overflow-y-auto border-l px-6 pb-6 pt-36 shadow-light-300 dark:shadow-none max-lg:hidden">
       <div className="h-[50svh]">
-        <h3 className="base-bold">Hot Network</h3>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-4 flex flex-col gap-4">
+          {hotQuestions.map((question) => (
+            <div
+              key={question._id}
+              className="flex items-start justify-between"
+            >
+              <p className="text-dark200_light900 text-clip text-left">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                width={20}
+                height={20}
+                alt="arrow-right"
+                className="invert-colors"
+              />
+            </div>
+          ))}
+        </div>
       </div>
+
       <div className="h-[50svh]">
-        <h3 className="base-bold">Popular Tags</h3>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-4 flex flex-col gap-4">
+          {popularTags.map((tag) => (
+            <div key={tag._id} className="flex items-center justify-between">
+              <Link href={`/tags/${tag.name}`}>
+                <p className="text-dark200_light900 cursor-pointer rounded-xl bg-slate-400 px-3 py-2">
+                  {tag.name}
+                </p>
+              </Link>
+              <p className="text-dark200_light900 p-2">{tag.totalQuestions}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
