8f50556fea7a67049be34f8bd3bafa5d4328c1e6
huly <qq6308qq@gmail.com>
2024-03-19 16:46:53 +0800

    chore: simple Home template

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,51 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  return <UserButton afterSignOutUrl="/" />;
+  return (
+    <>
+      <div className="flex w-full justify-between gap-4 max-sm:flex-col-reverse">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex flex-col items-center justify-center gap-5">
+        <div className="flex w-full items-center justify-between">
+          <p className="border px-4 py-2">Local Searchbar</p>
+          <p className="border px-4 py-2">Filters</p>
+        </div>
+        <div className="flex w-full flex-col gap-6">
+          {[
+            {
+              _id: 1,
+              title: "a",
+              content: "abcdefg",
+            },
+            {
+              _id: 2,
+              title: "b",
+              content: "abcdefg",
+            },
+            {
+              _id: 3,
+              title: "c",
+              content: "abcdefg",
+            },
+          ].map((article) => (
+            <div key={article._id} className="rounded-xl border px-4 py-2">
+              <h3 className="h3-bold">{article.title}</h3>
+              <p>{article.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,35 +3,15 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
-import { useTheme } from "@/context/ThemeProvider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { mode, setMode } = useTheme();
-
   return (
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
         <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <p className="dark:text-white">Theme Mode: {mode}</p>
-          <br />
-          <button
-            className="h-12 w-20 rounded-xl bg-orange-400 hover:bg-orange-600 dark:bg-orange-600 dark:text-white dark:hover:bg-orange-400"
-            onClick={() => {
-              if (mode === "light") {
-                setMode("dark");
-                localStorage.theme = "dark";
-              } else {
-                setMode("light");
-                localStorage.theme = "light";
-              }
-            }}
-          >
-            change
-          </button>
-          <br />
-          <div className="mx-auto w-full max-w-5xl bg-pink-300">{children}</div>
+          <div className="mx-auto w-full max-w-5xl ">{children}</div>
         </section>
         <RightSidebar />
       </div>
