d64776bbd0c1747a1c31a973d01b72d025516e15
huly <qq6308qq@gmail.com>
2024-03-21 19:33:36 +0800

    chore: move Title of Page from 'form component' to 'question page'

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -3,6 +3,10 @@ import Question from "@/components/form/Question";
 const page = () => {
   return (
     <div>
+      <h1 className="h1-bold text-dark100_light900 mb-4">
+        Ask a public question
+      </h1>
+
       <Question />
     </div>
   );
--- components/form/Question.tsx
+++ components/form/Question.tsx
@@ -79,156 +79,150 @@ const Question = () => {
   };
 
   return (
-    <>
-      <h2 className="h2-bold text-dark400_light800 mb-4">
-        Ask a public question
-      </h2>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex w-full flex-col items-center justify-center gap-10"
-        >
-          {/* Title */}
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                  Question Title <span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl className="mt-2">
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col items-center justify-center gap-10"
+      >
+        {/* Title */}
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-2">
+                <Input
+                  required
+                  placeholder="shadcn"
+                  className="background-light900_dark300 light-border min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage className="text-red-900" />
+            </FormItem>
+          )}
+        />
+
+        {/* Explanation */}
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-2">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "code",
+                      "help",
+                      "wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | " +
+                      "codesample | bold italic forecolor | " +
+                      "alignleft aligncenter alignright alignjustify | " +
+                      "bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2 text-light-500">
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className="text-red-900" />
+            </FormItem>
+          )}
+        />
+
+        {/* Tags */}
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-2">
+                <>
                   <Input
                     required
-                    placeholder="shadcn"
+                    placeholder="Add tags..."
                     className="background-light900_dark300 light-border min-h-[56px] border"
-                    {...field}
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
-                </FormControl>
-                <FormDescription className="body-regular mt-2 text-light-500">
-                  Be specific and imagine you&apos;re asking a question to
-                  another person.
-                </FormDescription>
-                <FormMessage className="text-red-900" />
-              </FormItem>
-            )}
-          />
 
-          {/* Explanation */}
-          <FormField
-            control={form.control}
-            name="explanation"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                  Detailed explanation of your problem{" "}
-                  <span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl className="mt-2">
-                  <Editor
-                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                    onInit={(evt, editor) => {
-                      // @ts-ignore
-                      editorRef.current = editor;
-                    }}
-                    initialValue=""
-                    init={{
-                      height: 350,
-                      menubar: false,
-                      plugins: [
-                        "advlist",
-                        "autolink",
-                        "lists",
-                        "link",
-                        "image",
-                        "charmap",
-                        "preview",
-                        "anchor",
-                        "searchreplace",
-                        "visualblocks",
-                        "codesample",
-                        "fullscreen",
-                        "insertdatetime",
-                        "media",
-                        "table",
-                        "code",
-                        "help",
-                        "wordcount",
-                      ],
-                      toolbar:
-                        "undo redo | " +
-                        "codesample | bold italic forecolor | " +
-                        "alignleft aligncenter alignright alignjustify | " +
-                        "bullist numlist",
-                      content_style:
-                        "body { font-family:Inter; font-size:16px }",
-                    }}
-                  />
-                </FormControl>
-                <FormDescription className="body-regular mt-2 text-light-500">
-                  Introduce the problem and expand on what you put in the title.
-                  Minimum 20 characters.
-                </FormDescription>
-                <FormMessage className="text-red-900" />
-              </FormItem>
-            )}
-          />
-
-          {/* Tags */}
-          <FormField
-            control={form.control}
-            name="tags"
-            render={({ field }) => (
-              <FormItem className="flex w-full flex-col">
-                <FormLabel className="paragraph-semibold text-dark400_light800">
-                  Tags <span className="text-primary-500">*</span>
-                </FormLabel>
-                <FormControl className="mt-2">
-                  <>
-                    <Input
-                      required
-                      placeholder="Add tags..."
-                      className="background-light900_dark300 light-border min-h-[56px] border"
-                      onKeyDown={(e) => handleInputKeyDown(e, field)}
-                    />
-
-                    {/* Display Tags */}
-                    {field.value.length > 0 && (
-                      <div className="mt-2 flex items-center gap-2">
-                        {field.value.map((tag: any) => (
-                          <Badge
-                            key={tag}
-                            className="subtle-medium text-light400_light500 background-light800_dark300 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
-                          >
-                            {tag}
-                            <Image
-                              src="/assets/icons/close.svg"
-                              alt="delete tag"
-                              width={12}
-                              height={12}
-                              className="cursor-pointer object-contain dark:invert"
-                              onClick={() => handleTagRemove(tag, field)}
-                            />
-                          </Badge>
-                        ))}
-                      </div>
-                    )}
-                  </>
-                </FormControl>
-                <FormDescription className="body-regular mt-2 text-light-500">
-                  Add up to 5 tags to describe what your question is about.
-                  Start typing to see suggestions.
-                </FormDescription>
-                <FormMessage className="text-red-900" />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="border shadow">
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </>
+                  {/* Display Tags */}
+                  {field.value.length > 0 && (
+                    <div className="mt-2 flex items-center gap-2">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium text-light400_light500 background-light800_dark300 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="delete tag"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain dark:invert"
+                            onClick={() => handleTagRemove(tag, field)}
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
+              </FormControl>
+              <FormDescription className="body-regular mt-2 text-light-500">
+                Add up to 5 tags to describe what your question is about. Start
+                typing to see suggestions.
+              </FormDescription>
+              <FormMessage className="text-red-900" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="border shadow">
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 };
 
