ddb80c58bf8729faeeb4fa2527fafd6dc2b7212c
huly <qq6308qq@gmail.com>
2024-03-26 16:22:54 +0800

    chore: create User model

---
+++ database/user.model.ts
@@ -0,0 +1,35 @@
+import { Document, model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  pictire: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinAt: Date;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, require: true },
+  name: { type: String, require: true },
+  username: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String },
+  bio: { type: String },
+  pictire: { type: String, require: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  joinAt: { type: Date, default: Date.now },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
