ae1e69ed6bd21147c0236309cadfceed18a378ac
huly <qq6308qq@gmail.com>
2024-03-27 17:01:39 +0800

    chore: append Types of Params about User

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -2,6 +2,8 @@ import { Schema } from "mongoose";
 
 import type { IUser } from "@/database/user.model";
 
+/* Question */
+
 export interface GetQuestionsParams {
   page?: number;
   pageSize?: number;
@@ -15,4 +17,39 @@ export interface CreateQuestionParams {
   tags: string[];
   author: Schema.Types.ObjectId | IUser;
   path: string;
+}
+
+/* User */
+export interface GetUserByIdParams {
+  userId: string;
+}
+
+export interface GetAllUserParams {
+  page?: number;
+  pageSize?: number;
+  filter?: string;
+  searchQuery?: string;
+}
+
+export interface GetUserStatsParams {
+  userId: string;
+  page?: string;
+  pageSize?: string;
+}
+
+export interface CreateUserParams {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}
+
+export interface UpdateUserParams {
+  clerkId: string;
+  updateData: Partial<IUser>;
+}
+
+export interface DeleteUserParams {
+  clerkId: string;
 }
