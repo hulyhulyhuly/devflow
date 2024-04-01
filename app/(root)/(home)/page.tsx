96cf2a22d74a1b263c23d5734de86a9eb49c2f8e
huly <qq6308qq@gmail.com>
2024-04-01 18:43:59 +0800

    chore: delete mock data about questions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,52 +1,13 @@
 import Link from "next/link";
 
+import { HomePageFilters } from "@/constants/filter";
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import { Button } from "@/components/ui/button";
-import { HomePageFilters } from "@/constants/filter";
 import NotResult from "@/components/shared/NotResult";
-import QuestionCard from "@/components/cards/QuestionCard";
+import { Button } from "@/components/ui/button";
 import { getQuestions } from "@/lib/actions/question.action";
-
-/* const questions = [
-  {
-    _id: "1",
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "john-doe.jpg",
-    },
-    upvotes: 1500000,
-    views: 500552,
-    answers: 10,
-    // answers: [],
-    createdAt: new Date("2023-09-01T12:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to center a div?",
-    tags: [
-      { _id: "3", name: "css" },
-      { _id: "4", name: "html" },
-    ],
-    author: {
-      _id: "2",
-      name: "Jane Smith",
-      picture: "jane-smith.jpg",
-    },
-    upvotes: 5,
-    views: 50,
-    answers: 3,
-    // answers: [],
-    createdAt: new Date("2021-09-02T10:30:00.000Z"),
-  },
-]; */
 
 const Home = async () => {
   const result = await getQuestions({});
