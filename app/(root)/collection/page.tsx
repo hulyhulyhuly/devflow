e7838f3188adf34154a86fa8c3514084e20750ff
huly <qq6308qq@gmail.com>
2024-04-03 21:23:17 +0800

    feat: Collection Page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,5 +1,78 @@
-const page = () => {
-  return <div>Collection</div>;
+import { auth } from "@clerk/nextjs";
+
+import QuestionCard from "@/components/cards/QuestionCard";
+import Filter from "@/components/shared/Filter";
+import NotResult from "@/components/shared/NotResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { QuestionFilters } from "@/constants/filter";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+
+const page = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
+      <div className="mt-10 flex w-full justify-between gap-4 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={QuestionFilters}
+          innerClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map(
+            (question: {
+              _id: string;
+              title: string;
+              tags: { _id: string; name: string }[];
+              author: { _id: string; name: string; picture: string };
+              upvotes: string[];
+              views: number;
+              answers: object[];
+              createdAt: Date;
+            }) => (
+              <QuestionCard
+                key={question._id}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                views={question.views}
+                answers={question.answers}
+                createdAt={question.createdAt}
+              />
+            )
+          )
+        ) : (
+          <NotResult
+            title="There’s no question saved to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default page;
