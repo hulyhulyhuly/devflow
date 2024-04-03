90ea6e1c5632dbec0778439b544e0cf2ded98a36
huly <qq6308qq@gmail.com>
2024-04-03 21:46:29 +0800

    chore: add viewQuestion Action && its params Type

---
+++ lib/actions/interaction.action.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import type { ViewQuestionParams } from "@/lib/actions/shared.types";
+import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
+import Interaction from "@/database/interaction.model";
+
+export async function viewQuestion(params: ViewQuestionParams) {
+  try {
+    await connectToDatabase();
+
+    const { questionId, userId } = params;
+
+    // Update view count for the question
+    await Question.findByIdAndUpdate(questionId, { $inc: { views: 1 } });
+
+    if (userId) {
+      const existingInteraction = await Interaction.findOne({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+
+      if (existingInteraction) {
+        console.log("User has already viewed.");
+      }
+
+      // Create Interaction
+      await Interaction.create({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
