18ebf4e2fe14af365b525a75b15c3f867f7ee6e5
huly <qq6308qq@gmail.com>
2024-04-04 10:17:25 +0800

    feat: Improve user experience for non-logged-in users

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -2,17 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs";
 
-// import Answer from "@/components/form/Answer";
+import Answer from "@/components/form/Answer";
 import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
-import Answer from "@/components/form/Answer";
-import Votes from "@/components/shared/Votes";
 
 const page = async ({ params }: { params: { id: string } }) => {
   const result = await getQuestionById({ questionId: params.id });
@@ -28,38 +27,26 @@ const page = async ({ params }: { params: { id: string } }) => {
     <>
       <div className="flex w-full flex-col items-center">
         <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
-          <Link
-            href={`/profile/${result.author.clerkId}`}
-            className="flex items-center justify-start gap-1"
-          >
-            <Image
-              src={result.author.picture}
-              className="rounded-full"
-              width={22}
-              height={22}
-              alt="profile"
-            />
-            <p className="paragraph-semibold text-dark300_light700">
-              {result.author.name}
-            </p>
+          <Link href={`/profile/${result.author.clerkId}`} className="flex items-center justify-start gap-1">
+            <Image src={result.author.picture} className="rounded-full" width={22} height={22} alt="profile" />
+            <p className="paragraph-semibold text-dark300_light700">{result.author.name}</p>
           </Link>
 
           <div className="flex justify-end">
             <Votes
               itemType="question"
               itemId={JSON.stringify(result._id)}
-              userId={JSON.stringify(mongoUser._id)}
+              // userId={JSON.stringify(mongoUser._id)}
+              userId={mongoUser?._id ? JSON.stringify(mongoUser._id) : undefined}
               upvotes={result.upvotes.length}
-              hasUpVoted={result.upvotes.includes(mongoUser._id)}
+              hasUpVoted={result.upvotes.includes(mongoUser?._id)}
               downvotes={result.downvotes.length}
-              hasDownVoted={result.downvotes.includes(mongoUser._id)}
-              hasSaved={mongoUser.saved.includes(result._id)}
+              hasDownVoted={result.downvotes.includes(mongoUser?._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
             />
           </div>
         </div>
-        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
-          {result.title}
-        </h2>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">{result.title}</h2>
       </div>
 
       <div className="mb-8 mt-5 flex flex-wrap gap-4">
@@ -90,25 +77,13 @@ const page = async ({ params }: { params: { id: string } }) => {
 
       <div className="mt-8 flex flex-wrap gap-2">
         {result.tags.map((tag: any) => (
-          <RenderTag
-            key={tag._id}
-            _id={tag._id}
-            name={tag.name}
-            showCount={false}
-          />
+          <RenderTag key={tag._id} _id={tag._id} name={tag.name} showCount={false} />
         ))}
       </div>
 
-      <AllAnswers
-        questionId={result._id}
-        totalAnswers={result.answers.length}
-        userId={mongoUser._id}
-      />
+      <AllAnswers questionId={result._id} totalAnswers={result.answers.length} userId={mongoUser?._id} />
 
-      <Answer
-        authorId={JSON.stringify(mongoUser._id)}
-        questionId={JSON.stringify(result._id)}
-      />
+      {mongoUser && <Answer authorId={JSON.stringify(mongoUser._id)} questionId={JSON.stringify(result._id)} />}
     </>
   );
 };
