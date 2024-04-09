234e012baf85c84966d7e593752422ad8a9db3d2
huly <qq6308qq@gmail.com>
2024-04-09 18:06:43 +0800

    chore: adjust Styles, and use Tabs to build User's interacted records (Q&A)

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { getUserInfo } from "@/lib/actions/user.action";
-import { SignedIn, auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { SignedIn, auth } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { getUserInfo } from "@/lib/actions/user.action";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getJoinedDate } from "@/lib/utils";
 
 interface Props {
   params: {
@@ -30,9 +33,9 @@ const page = async ({ params: { id } }: Props) => {
             className="object-cover rounded-full"
           />
 
-          <div className="flex flex-col gap-4">
-            <h2 className="paragraph-semibold">{userInfo.user.name}</h2>
-            <p>@{userInfo.user.username}</p>
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{userInfo.user.name}</h2>
+            <p className="paragraph-regular text-dark200_light800">@{userInfo.user.username}</p>
 
             <div className="flex flex-wrap items-center max-md:items-start max-md:flex-col gap-2">
               <div className="flex items-center gap-1">
@@ -44,12 +47,17 @@ const page = async ({ params: { id } }: Props) => {
 
               <div className="flex items-center gap-1">
                 <Image src="/assets/icons/location.svg" alt="location" width={20} height={20} />
-                <p>{userInfo.user?.location ?? "Taipei, Taiwan"}</p>
+                {userInfo.user.location ?? (
+                  <p className="">
+                    {/* {userInfo.user.location} */}
+                    Taipei, Taiwan
+                  </p>
+                )}
               </div>
 
               <div className="flex items-center gap-1">
                 <Image src="/assets/icons/calendar.svg" alt="join" width={20} height={20} />
-                <p>Joined {userInfo.user.joinAt.toString()}</p>
+                <p>Joined {getJoinedDate(userInfo.user.joinAt)}</p>
               </div>
             </div>
 
@@ -64,7 +72,7 @@ const page = async ({ params: { id } }: Props) => {
         </div>
 
         {/* Edit Profile Button */}
-        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3 max-sm:justify-start max-sm:mt-5">
           <SignedIn>
             {clerkId === userInfo.user.clerkId && (
               <Link href="/profile/edit">
@@ -83,16 +91,25 @@ const page = async ({ params: { id } }: Props) => {
         <div>Prizes</div>
       </div>
 
-      {/* Questions & Top Tags */}
-      <div className="flex flex-1 justify-between border-2 border-yellow-500 mt-4">
-        {/* Questions */}
-        <div className="border border-green-600">
-          <div>Filter</div>
-          <div>Answers</div>
-          <div className="flex items-center justify-center">
-            <Button className="primary-gradient text-white">Load More</Button>
-          </div>
-        </div>
+      {/* Top Posts & Answers / Top Tags */}
+      <div className="flex flex-1 justify-between mt-4">
+        {/* Top Posts & Answers */}
+        <Tabs defaultValue="top-posts" className="flex-1">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+
+          {/* Top Posts (question by user) */}
+          <TabsContent value="top-posts">Many Questions by User</TabsContent>
+
+          {/* Answers */}
+          <TabsContent value="answers">Many Answers By User</TabsContent>
+        </Tabs>
 
         {/* Top Tags */}
         <div className="border border-indigo-500">
