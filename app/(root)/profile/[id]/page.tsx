65ced1edb53c8571ed857025e8a1ec8ead25852e
huly <qq6308qq@gmail.com>
2024-04-11 20:45:15 +0800

    chore: add Edit Page of Profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -66,7 +66,7 @@ const page = async ({ params: { id } }: Props) => {
         <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3 max-sm:justify-start max-sm:mt-5">
           <SignedIn>
             {clerkId === userInfo.user.clerkId && (
-              <Link href="/profile/edit">
+              <Link href={`/profile/${id}/edit`}>
                 <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
                   Edit Profile
                 </Button>
