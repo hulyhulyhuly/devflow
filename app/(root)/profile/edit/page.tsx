2b68e366d47d60ba210744861d88a116984665bc
huly <qq6308qq@gmail.com>
2024-04-12 13:04:05 +0800

    fix: move dir 'profile/[id]/edit' -> 'profile/edit'

---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs";
+
+import Profile from "@/components/form/Profile";
+import { getUserById } from "@/lib/actions/user.action";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const Page = async ({ params }: Props) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
+
+      <div className="mt-10">
+        <Profile clerkId={userId} user={mongoUser} />
+      </div>
+    </>
+  );
+};
+
+export default Page;
